dbc9b09667f1f6dfa91c897f7dd05f13709c3816
veronicawong3042 <150846086+veronicawong3042@users.noreply.github.com>
2024-02-17 12:45:35 -0800

    registered staff cpt and taxonomy

--- functions.php
+++ functions.php
@@ -169,6 +169,8 @@ require get_template_directory() . '/inc/template-functions.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
+require get_template_directory() . '/inc/cpt-taxonomy.php';
+
 /**
  * Load Jetpack compatibility file.
  */
---
+++ inc/cpt-taxonomy.php
@@ -0,0 +1,84 @@
+<?php
+function fwd_register_custom_post_types() {
+    // Staff CPT
+    $labels = array(
+        'name'                  => _x( 'Staff', 'post type general name' ),
+        'singular_name'         => _x( 'Staff', 'post type singular name'),
+        'menu_name'             => _x( 'Staff', 'admin menu' ),
+        'name_admin_bar'        => _x( 'Staff', 'add new on admin bar' ),
+        'add_new'               => _x( 'Add New', 'staff' ),
+        'add_new_item'          => __( 'Add New Staff' ),
+        'new_item'              => __( 'New Staff' ),
+        'edit_item'             => __( 'Edit Staff' ),
+        'view_item'             => __( 'View Staff' ),
+        'all_items'             => __( 'All Staff' ),
+        'search_items'          => __( 'Search Staff' ),
+        'parent_item_colon'     => __( 'Parent Staff:' ),
+        'not_found'             => __( 'No works found.' ),
+        'not_found_in_trash'    => __( 'No works found in Trash.' ),
+        'archives'              => __( 'Staff Archives'),
+        'insert_into_item'      => __( 'Insert into staff'),
+        'uploaded_to_this_item' => __( 'Uploaded to this staff'),
+        'filter_item_list'      => __( 'Filter staff list'),
+        'items_list_navigation' => __( 'Staff list navigation'),
+        'items_list'            => __( 'Staff list'),
+        'featured_image'        => __( 'Staff featured image'),
+        'set_featured_image'    => __( 'Set staff featured image'),
+        'remove_featured_image' => __( 'Remove staff featured image'),
+        'use_featured_image'    => __( 'Use as featured image'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'staff' ),
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'menu_icon'          => 'dashicons-archive',
+        'supports'           => array( 'title'),
+    );
+
+    register_post_type( 'fwd-staff', $args );
+}
+
+add_action( 'init', 'fwd_register_custom_post_types' );
+
+function fwd_register_taxonomies() {
+    // Add Staff CPT Taxonomy
+    $labels = array(
+        'name'              => _x( 'Staff Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Staff Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Staff' ),
+        'all_items'         => __( 'All Staff' ),
+        'parent_item'       => __( 'Parent Staff' ),
+        'parent_item_colon' => __( 'Parent Staff:' ),
+        'edit_item'         => __( 'Edit Staff' ),
+        'view_item'         => __( 'View Staff' ),
+        'update_item'       => __( 'Update Staff' ),
+        'add_new_item'      => __( 'Add New Staff' ),
+        'new_item_name'     => __( 'New Staff Name' ),
+        'menu_name'         => __( 'Staff Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'staff-categories' ),
+    );
+    register_taxonomy( 'fwd-staff-category', array( 'fwd-staff' ), $args );
+}
+add_action( 'init', 'fwd_register_taxonomies');
